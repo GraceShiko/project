d26bd1f0d909cb8d556a341a4155cd371060f951
shiko <graciewshiko@gmail.com>
2021-07-23 11:02:54 +0300

    done

--- track/settings/secrets_dev.py
+++ track/settings/secrets_dev.py
@@ -7,3 +7,7 @@ DATABASES = {
        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    }
 }
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
